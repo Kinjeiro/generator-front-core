3355f790b5765329c8616e5cf83bf5333f1b262d
Andrey Kuzmin <kinjeiro@gmail.com>
2019-08-28 12:39:22 +0300

    feat(module-crud): - добавил примеры для серверной части

--- generators/module/templates/1.7/entitiesModuleTemplate/server/Service$$moduleNameCapital$$.js
+++ generators/module/templates/1.7/entitiesModuleTemplate/server/Service$$moduleNameCapital$$.js
@@ -2,12 +2,18 @@ import { objectValues } from '@reagentum/front-core/lib/common/utils/common';
 
 import CoreService from '@reagentum/front-core/lib/server/services/utils/CoreService';
 
+/*
+ * Если в serverConfig.server.endpointServices[serviceName] нету используется
+ * по умолчанию serverConfig.server.endpointServices.middlewareApiService
+ */
 export default class Service<%=moduleNameCapital%> extends CoreService {
   async load<%=moduleNameCapital%>(query = {}) {
+    // return this.send('/products', query);
     throw new Error('todo');
   }
 
   async load<%=entityNameCapital%>(id) {
+    // return this.sendWithApi(`/products/${id}`);
     throw new Error('todo');
   }
 
