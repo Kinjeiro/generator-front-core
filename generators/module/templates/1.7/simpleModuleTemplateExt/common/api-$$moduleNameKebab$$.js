a491ad830707734f260489e64bd26f356d5835e2
Andrey Kuzmin <kinjeiro@gmail.com>
2019-09-20 16:00:26 +0300

    feat(app): - вынес send-api из module-core

--- generators/module/templates/1.7/simpleModuleTemplateExt/common/api-$$moduleNameKebab$$.js
+++ generators/module/templates/1.7/simpleModuleTemplateExt/common/api-$$moduleNameKebab$$.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 import apiConfig from '@reagentum/front-core/lib/common/utils/create-api-config';
 
-import sendApi from '../../../modules/module-core/utils/send-api';
+import sendApi from '../../../common/send-api';
 
 export const API_PREFIX = '<%=moduleNameCamel%>';
 export const API_CONFIGS = {
   load<%=moduleNameCapital%>: apiConfig(`/${API_PREFIX}`, 'GET'),
-};
+}
 
 export function apiLoad<%=moduleNameCapital%>() {
   return sendApi(API_CONFIGS.load<%=moduleNameCapital%>);
